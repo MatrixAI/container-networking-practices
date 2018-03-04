fe86e75f2157c49b43ae370b291991ea31cd999b
Moku <mokuki082@gmail.com>
2018-03-04 18:32:37 +1100

    Deleted bridges (not needed)

--- simple_isolation.sh
+++ simple_isolation.sh
@@ -6,52 +6,41 @@
 # Clean up
 
 cleanup() {
+  exec 2>/dev/null
   # Clean up namespaces
   sudo ip netns delete cont1 || true
   sudo ip netns delete cont2 || true
   sudo ip netns delete cont3 || true
 
-  # Clean up host bridges
-  sudo ifconfig bridge0 down
-  sudo brctl delbr bridge0
-
   echo successful cleanup
+  exec 2>2
 }
 
 cleanup
 
-export HOST_BRIDGE0_IP="10.0.3.1/24"
-export CONT1_ETH0_IP="10.0.3.2/24"
-export CONT2_BRIDGE0_IP="10.0.4.1/24"
-export CONT3_ETH0_IP="10.0.4.2/24"
+export NS0_VETH0_IP="10.0.3.1/24"
+export NS1_VETH1_IP="10.0.3.2/24"
+export NS2_VETH0_IP="10.0.4.1/24"
+export NS3_VETH0_IP="10.0.4.2/24"
 
 # Create network namespaces for container 1, 2 and 3.
 sudo ip netns add cont1
 sudo ip netns add cont2
 sudo ip netns add cont3
 
-# Create a bridge to be used at host vm
-sudo brctl addbr bridge0
-sudo ifconfig bridge0 "$HOST_BRIDGE0_IP" up
 
 # Create a link between host and cont1
 sudo ip netns exec cont1 ip link add veth0 type veth peer name eth0
-sudo ip netns exec cont1 ifconfig eth0 "$CONT1_ETH0_IP" up
+sudo ip netns exec cont1 ifconfig eth0 "$NS1_VETH0_IP" up
 sudo ip netns exec cont1 ip link set veth0 netns 1
-sudo ifconfig veth0 up
-sudo brctl addif bridge0 veth0
+sudo ifconfig veth0 "$NS0_VETH0_IP" up
 
 echo link between host and cont1 set
 
-# Create a bridge to be used at cont2
-sudo ip netns exec cont2 brctl addbr bridge0
-sudo ip netns exec cont2 ifconfig bridge0 "$CONT2_BRIDGE0_IP" up
-
 # Create a link between cont2 and cont3
 sudo ip netns exec cont3 ip link add veth0 type veth peer name eth0
-sudo ip netns exec cont3 ifconfig eth0 "$CONT3_ETH0_IP" up
+sudo ip netns exec cont3 ifconfig eth0 "$NS3_VETH0_IP" up
 sudo ip netns exec cont3 ip link set veth0 netns cont2
-sudo ip netns exec cont2 ifconfig veth0 up
-sudo ip netns exec cont2 brctl addif bridge0 veth0
+sudo ip netns exec cont2 ifconfig veth0 "$NS2_VETH0_IP" up
 
 echo link between cont2 and cont3 set
