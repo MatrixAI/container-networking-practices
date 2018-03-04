ab7e0d8dad08413a50508601ba601c9540285c86
Moku <mokuki082@gmail.com>
2018-03-04 15:05:01 +1100

    Created simple_isolation.sh

--- simple_isolation.sh
+++ simple_isolation.sh
@@ -3,11 +3,27 @@
 #   2. container 1 is routable publically
 #   3. container 2 and 3 can only talk to each other.
 
-env HOST_BRIDGE_IP="10.0.3.0/24"
-env CONT1_HOST_IP="10.0.3.1/24"
-env CONT1_BRIDGE_IP="10.0.4.0/24"
-env CONT2_CONT1_IP="10.0.4.1/24"
-env CONT3_CONT1_IP="10.0.4.2/24"
+# Clean up
+
+cleanup() {
+  # Clean up namespaces
+  sudo ip netns delete cont1 || true
+  sudo ip netns delete cont2 || true
+  sudo ip netns delete cont3 || true
+
+  # Clean up host bridges
+  sudo ifconfig bridge0 down
+  sudo brctl delbr bridge0
+
+  echo successful cleanup
+}
+
+cleanup
+
+export HOST_BRIDGE0_IP="10.0.3.1/24"
+export CONT1_ETH0_IP="10.0.3.2/24"
+export CONT2_BRIDGE0_IP="10.0.4.1/24"
+export CONT3_ETH0_IP="10.0.4.2/24"
 
 # Create network namespaces for container 1, 2 and 3.
 sudo ip netns add cont1
@@ -16,26 +32,26 @@ sudo ip netns add cont3
 
 # Create a bridge to be used at host vm
 sudo brctl addbr bridge0
-sudo ifconfig bridge0 "10.0.3.0/24" up
+sudo ifconfig bridge0 "$HOST_BRIDGE0_IP" up
 
-creat_pair() {
-  THIS_NS=$1 # Namespace of which the veth pair should be created
-  OTHER_NS=$1 # Namespace of whcih the veth pair is joining to
-  VETH0_NAME=$2 # name of the veth pair that stays in THIS_NS
-  VETH1_NAME=$3 # name of the veth pair that is passed to OTHER_NS
+# Create a link between host and cont1
+sudo ip netns exec cont1 ip link add veth0 type veth peer name eth0
+sudo ip netns exec cont1 ifconfig eth0 "$CONT1_ETH0_IP" up
+sudo ip netns exec cont1 ip link set veth0 netns 1
+sudo ifconfig veth0 up
+sudo brctl addif bridge0 veth0
 
-  # Create a veth pair to be used in between host and cont1
-  sudo ip netns exec $THIS_NS ip link add veth0 type veth peer name eth0
-  # Configure addresses, namespaces and fire them up
-  sudo ip netns exec $THIS_NS ifconfig eth0 "$CONT1_HOST_IP" up
-  sudo ip netns exec $THIS_NS ip link set veth0 netns $OTHER_NS
-  sudo ip netns exec $OTHER_NS ip link set veth0 up
+echo link between host and cont1 set
 
-  # Connect veth0 to the bridge
-  sudo ip netns exec $OTHER_NS brctl addif bridge0 veth0
-}
+# Create a bridge to be used at cont2
+sudo ip netns exec cont2 brctl addbr bridge0
+sudo ip netns exec cont2 ifconfig bridge0 "$CONT2_BRIDGE0_IP" up
 
-join_pair()
+# Create a link between cont2 and cont3
+sudo ip netns exec cont3 ip link add veth0 type veth peer name eth0
+sudo ip netns exec cont3 ifconfig eth0 "$CONT3_ETH0_IP" up
+sudo ip netns exec cont3 ip link set veth0 netns cont2
+sudo ip netns exec cont2 ifconfig veth0 up
+sudo ip netns exec cont2 brctl addif bridge0 veth0
 
-
-sudo ip link add veth0 type veth peer name
+echo link between cont2 and cont3 set
