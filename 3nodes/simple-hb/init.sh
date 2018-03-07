3f50deab2fb96c2b0db774dc886a8612da26c20f
Moku <mokuki082@gmail.com>
2018-03-07 19:12:38 +1100

    Hide verbose messages

--- 3nodes/simple-hb/init.sh
+++ 3nodes/simple-hb/init.sh
@@ -3,8 +3,8 @@
 ###
 
 cleanup() {
-  docker stop peer1 peer2 peer3
-  docker rm peer1 peer2 peer3
+  docker stop peer1 peer2 peer3 2> /dev/null
+  docker rm peer1 peer2 peer3 2> /dev/null
   docker network rm simple-hb-br 2> /dev/null
 }
 
@@ -29,14 +29,14 @@ echo Dockerfile written
 
 cleanup
 
-docker build -t simple-hb .
+docker build -t simple-hb . > /dev/null
 
 echo Docker image built
 
 docker network create \
   -d bridge \
   --subnet=10.0.4.1/24 \
-  simple-hb-br
+  simple-hb-br > /dev/null
 
 echo Network simple-hb-br created
 
@@ -44,7 +44,7 @@ sudo docker run -d --name peer1 \
   --network simple-hb-br \
   --ip "10.0.4.21" \
   --publish 8080:8080 \
-  simple-hb > peer1.log
+  simple-hb > /dev/null
 
 echo peer1 running
 
@@ -52,7 +52,7 @@ sudo docker run -d --name peer2 \
   --network simple-hb-br \
   --ip "10.0.4.22" \
   --publish 8081:8080 \
-  simple-hb > peer2.log
+  simple-hb > /dev/null
 
 echo peer2 running
 
@@ -60,6 +60,6 @@ sudo docker run -d --name peer3 \
   --network simple-hb-br \
   --ip "10.0.4.23" \
   --publish 8082:8080 \
-  simple-hb > peer3.log
+  simple-hb > /dev/null
 
 echo peer3 running
