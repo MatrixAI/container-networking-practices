5be92beb88c0e65e418fd1c04b98b69213d76dbf
Moku <mokuki082@gmail.com>
2018-03-10 18:55:50 +1100

    Tried to implement nat with iptables but failing

--- experiments/vpn_veth.sh
+++ experiments/vpn_veth.sh
@@ -4,19 +4,19 @@
 
 # Clean up
 cleanup() {
-  ip netns del A-dhcp 2> /dev/null
+  ip netns del A-router 2> /dev/null
   ip netns del A-node 2> /dev/null
   ip netns del B-router 2> /dev/null
-  ip netns del B-node 2> /dev/null
 }
 
-# configure-network-A
+# configure network A
 
   cleanup
 
   # Create namespaces for network A
   ip netns add A-router
   ip netns add A-node
+  ip netns add B-router
 
   # Routers have bridges to join the networks via veth pairs
   ip netns exec A-router brctl addbr bridge0
@@ -27,7 +27,7 @@ cleanup() {
 
   # Setting up dhcp using `dnsmasq`
   ip netns exec A-router dnsmasq \
-    --dhcp-range=10.0.3.2,10.0.3.254,255.255.255.0 --interface=bridge0
+    --dhcp-range=10.0.3.3,10.0.3.254,255.255.255.0 --interface=bridge0
 
   # Linking A-router and A-node
   ip netns exec A-router ip link add eth0 type veth peer name veth0
@@ -39,34 +39,30 @@ cleanup() {
   ip netns exec A-node ip link set eth0 up
   ip netns exec A-node dhclient -v eth0
 
-
-# configure-network-B
-
-  ip netns add B-router
+# configure network B
   ip netns exec B-router brctl addbr bridge0
-  ip netns exec B-router ifconfig bridge0 "10.0.4.1/24" up
+  ip netns exec B-router ifconfig bridge0 "10.0.4.0/24" up
 
 # configure router
 
   # Set up a default gateway for A-router
-  ip netns exec A-router ip link add eth0 type veth peer name tar0
+  ip netns exec A-router ip link add eth0 type veth peer name veth2
+
   # Bring up the devices
-  ip netns exec A-router
+  ip netns exec A-router ifconfig eth0 "10.0.5.1" up # Gateway
+  ip netns exec A-router ip link set veth2 netns B-router
+  ip netns exec B-router ip link set veth2 up
+  ip netns exec B-router brctl addif bridge0 veth2
+
+  # Set A-route's default gateway to "10.0.5.1"
+  ip netns exec A-router ip route add default via "10.0.5.1"
 
   # Configure NAT and port forwarding
   sysctl net.ipv4.conf.all.forwarding=1
-
   ip netns exec A-router iptables --flush
-  # Nat flush must be explicity stated
+  ip netns exec A-router iptables --flush
   ip netns exec A-router iptables --table nat --flush
   ip netns exec A-router iptables --table nat --delete-chain
 
 
-  ip netns exec A-router iptables --table nat \
-                                  --append POSTROUTING \
-                                  -o eth0 \
-                                  -j MASQUERADE
-  # Place the target at B-router
-  ip netns exec A-router ip link tar0 set netns B-router
-
-  # Apply the configuration
+  ip netns exec A-router iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
